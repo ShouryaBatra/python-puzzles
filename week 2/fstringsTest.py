b0d28a8505d10e574b5e94324b71050b90bcaeee
bublibub <shourya.batra@gmail.com>
2023-06-10 16:41:36 -0700

    Create fstringsTest.py

---
+++ week 2/fstringsTest.py
@@ -0,0 +1,8 @@
+name = "Shourya"
+age = 420
+favoriteColor = "red"
+fewDigitsOfPi = 3.14159265
+
+print(f"my name is {name}. \
+i am {age} years old. \n\
+my favorite color is {favoriteColor}. \nhere are a few digits of pi: {fewDigitsOfPi}")
