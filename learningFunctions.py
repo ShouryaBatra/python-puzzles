321d7518a9c29c1de249907a225a01fa24b92b98
bublibub <shourya.batra@gmail.com>
2023-07-08 17:33:21 -0700

    made function to draw triangle

--- learningFunctions.py
+++ learningFunctions.py
@@ -1,4 +1,24 @@
 def fToC(F):
     return ((F-32) * 5) / 9
-for i in range(10, 101, 10):
-    print(fToC(i))
+# for i in range(10, 101, 10):
+#     print(fToC(i))
+
+# if 2: then draw:
+# *
+# **
+
+# if 3:
+# *
+# **
+# ***
+
+def drawTriangle(num):
+    for i in range(1, num + 1):
+        # print("*" * i)
+        for j in range(i):
+            print("*", end = "")
+        print()
+
+drawTriangle(5)
+print()
+drawTriangle(10)
