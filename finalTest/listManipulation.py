009cd5d5098ce8e528a4c87ec1800e4e3828f3df
ShouryaBatra <shourya.batra@gmail.com>
2023-07-31 20:11:21 -0700

    fixed printing inside functions

--- finalTest/listManipulation.py
+++ finalTest/listManipulation.py
@@ -3,36 +3,55 @@ def get_average(numbers: list[float]) -> float:
     for i in range(len(numbers)):
         sum = sum + numbers[i]
     average = sum / len(numbers)
-    print(average)
+    return average
 
-get_average([2,4,6,8,10])
 def get_max(numbers: list[int]) -> int:
     max = None
     for i in range(len(numbers)):
         if max == None or numbers[i] > max:
             max = numbers[i]
-    print(max)
-get_max([2,4,6,8,10])
+    return max
 
 def get_min(numbers: list[int]) -> int:
     min = None
     for i in range(len(numbers)):
         if min == None or numbers[i] < min:
             min = numbers[i]
-    print(min)
-get_min([2,4,6,8,10])
+    return min
 
 def get_even(numbers: list[int]) -> list[int]:
     evens = []
     for i in range(len(numbers)):
         if numbers[i] % 2 == 0:
             evens.append(numbers[i])
-    print(evens)
-get_even([3,12,7,6,9])
+    return evens
+    
 def get_odd(numbers: list[int]) -> list[int]:
     odds = []
     for i in range(len(numbers)):
         if numbers[i] % 2 == 1:
             odds.append(numbers[i])
-    print(odds)
-get_odd([3,12,7,6,9])
+    return odds
+
+# print(get_average([2,4,6,8,10]))
+# print(get_max())
+# print(get_min([2,4,6,8,10]))
+# print(get_even([3,12,7,6,9]))
+# print(get_odd([3,12,7,6,9]))
+
+numbers1 = [2, 4, 6, 8, 10]
+
+print(get_average(numbers1))
+print(get_max(numbers1))
+print(get_min(numbers1))
+print(get_even(numbers1))
+print(get_odd(numbers1))
+
+print()
+numbers2 = [3, 1, 7, 5, 9]
+
+print(get_average(numbers2))
+print(get_max(numbers2))
+print(get_min(numbers2))
+print(get_even(numbers2))
+print(get_odd(numbers2))
